e935588af4c397de51c6f29d9daa4bab6ee39989
321freddy <fred.ru.7@gmail.com>
2019-03-06 00:02:29 +0100

    Removed try catch

--- framework.lua
+++ framework.lua
@@ -4,28 +4,6 @@ scripts = {} -- custom scripts
 local funcs = {} -- Functions bound to their respective game event
 
 
--- Try catch control structure
-local trySuccess = { catch = function() end }
-local tryError   = { catch = function(catch) catch() end }
-
-function try(func, ...) -- only catches assert errors
-    local status, err = pcall(func, ...)
-    
-    if status then           -- no error
-        return trySuccess
-    else
-        if err == false then -- assert error
-            return tryError
-
-		else                 -- real error
-			status, err = xpcall(func, debug.traceback, ...)
-            error(err, 2)
-        end
-    end
-end
-local try = try
-
-
 local function formatLuaObject(v, handler)
 	if v.__self and type(v.__self) == "userdata" then
 		if v.valid == true then
@@ -118,7 +96,7 @@ local function registerFunc(name, id)
 end
 
 function handleEvent(event) -- Calls all script-functions with the same name as the game event that was just triggered
-	for __,func in pairs(funcs[event.input_name or event.name]) do try(func, event) end
+	for __,func in pairs(funcs[event.input_name or event.name]) do func(event) end
 end
 
 local function registerHandler(name, id) -- Register appropriate handler functions for game events if needed
