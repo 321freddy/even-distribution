20fb8c794b67d2f5478a8864e9668a0a1373340a
321freddy <fred.ru.7@gmail.com>
2019-08-18 15:45:58 +0200

    gui styles

--- data.lua
+++ data.lua
@@ -37,3 +37,63 @@ style.ed_settings_inner_frame = {
 	parent = "frame",
 	horizontally_stretchable = "on",
 }
+
+
+style.ed_switch_button = {
+	type = "button_style",
+	parent = "button",
+	left_padding = 0,
+	right_padding = 0,
+
+	hovered_font_color = button_default_font_color,
+	hovered_graphical_set =
+	{
+		base = {position = {0, 17}, corner_size = 8},
+		shadow = default_dirt
+	},
+	clicked_font_color = button_hovered_font_color,
+	clicked_graphical_set =
+	{
+		base = {position = {225, 17}, corner_size = 8},
+		shadow = default_dirt
+	},
+}
+
+style.ed_switch_button_selected = {
+	type = "button_style",
+	parent = "button",
+	left_padding = 0,
+	right_padding = 0,
+
+	-- selected button graphical sets as default
+	default_font_color = button_hovered_font_color,
+	default_graphical_set =
+	{
+		base = {position = {225, 17}, corner_size = 8},
+		shadow = default_dirt
+	},
+	-- hovered_font_color = button_hovered_font_color,
+	-- hovered_graphical_set =
+	-- {
+	-- 	base = {position = {369, 17}, corner_size = 8},
+	-- 	shadow = default_dirt
+	-- },
+	hovered_font_color = button_hovered_font_color,
+	hovered_graphical_set =
+	{
+		base = {position = {225, 17}, corner_size = 8},
+		shadow = default_dirt
+	},
+	-- clicked_font_color = button_hovered_font_color,
+	-- clicked_graphical_set =
+	-- {
+	-- 	base = {position = {352, 17}, corner_size = 8},
+	-- 	shadow = default_dirt
+	-- },
+	clicked_font_color = button_hovered_font_color,
+	clicked_graphical_set =
+	{
+		base = {position = {225, 17}, corner_size = 8},
+		shadow = default_dirt
+	},
+}
