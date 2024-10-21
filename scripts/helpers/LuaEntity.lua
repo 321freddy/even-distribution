488b7fd118ae1fa0ddc40739e3836395f47161e4
321freddy <fred.ru.7@gmail.com>
2024-10-21 19:11:35 +0200

    Fixed furnace recipe not correctly detected

--- scripts/helpers/LuaEntity.lua
+++ scripts/helpers/LuaEntity.lua
@@ -12,7 +12,7 @@ function entity:isIgnored(player)
 end
 
 function entity:recipe()
-	return _(self.get_recipe() or (self.type == "furnace" and self.previous_recipe))
+	return _(self.get_recipe() or (self.type == "furnace" and self.previous_recipe and self.previous_recipe.name))
 end
 
 -- for turrets
